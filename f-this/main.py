7365009dad6aacece28b5b161b3e97ed089207cd
Alex Loomis <alex.v.loomis@gmail.com>
2015-09-25 20:39:23 -0400

    GetFilters & CreateFilter Handlers added

--- f-this/main.py
+++ f-this/main.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import json
+from google.appengine.ext import ndb
 
-class MainHandler(webapp2.RequestHandler):
+class Filter(ndb.Model):
+    name = ndb.StringProperty()
+    find = ndb.StringProperty()
+    replace = ndb.StringProperty()
+
+class GetFiltersHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        filters = Filter.query().order(Filter.name).fetch()
+        jsonFilters = []
+        for f in filters:
+            jsonFilters.append(f.to_dict())
+        self.response.headers["Content-Type"] = "application/json"
+        self.response.write(json.dumps(jsonFilters))
+
+class CreateFilterHandler(webapp2.RequestHandler):
+    def get(self):
+        name = self.request.get("name")
+        f = Filter(name=name, find="", replace="")
+        f.put()
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/getFilters', GetFiltersHandler),
+    ('/createFilter', CreateFilterHandler)
 ], debug=True)
